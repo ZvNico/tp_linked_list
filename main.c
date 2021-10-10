2c41f2925e1f91b9f1b4984635a34f6798f09cff
zvnico <nicolas.baconnier@efrei.net>
2021-10-10 20:24:42 +0200

    ex1 almost done (missing supprimer_val)

--- main.c
+++ main.c
@@ -2,11 +2,15 @@
 #include "src/linked_list/linked_list.h"
 
 int main() {
+    int x;
     maillon_t *liste = (maillon_t *) malloc(sizeof(maillon_t));
     liste->x = 5;
     liste->suiv = NULL;
     liste = ajouter_debut(liste, 10);
     liste = ajouter_fin(liste, 8);
+    liste = ajouter_fin(liste, 9);
+    liste = supprimer_debut(liste, &x);
+    liste = supprimer_fin(liste, &x);
     afficher(liste);
     return 0;
 }
--- src/linked_list/linked_list.c
+++ src/linked_list/linked_list.c
@@ -25,14 +25,41 @@ maillon_t *ajouter_fin(maillon_t *pliste, int val) {
 }
 
 maillon_t *rechercher(maillon_t *pliste, int val) {
-
+    maillon_t *temp = pliste;
+    while (temp != NULL) {
+        if (temp->x == val) {
+            return temp;
+        }
+        temp = temp->suiv;
+    }
+    return pliste;
 }
 
-maillon_t *supprimer_debut(maillon_t *pliste, int *pval) {}
+maillon_t *supprimer_debut(maillon_t *pliste, int *pval) {
+    maillon_t *temp = pliste->suiv;
+    *pval = pliste->x;
+    free(pliste);
+    return temp;
+}
 
-maillon_t *supprimer_fin(maillon_t *pliste, int *pval) {}
+maillon_t *supprimer_fin(maillon_t *pliste, int *pval) {
+    if (pliste == NULL) {
+        return pliste;
+    }
+    maillon_t *temp = pliste;
+    while (temp->suiv->suiv != NULL) {
+        temp = temp->suiv;
+    }
+    *pval = temp->suiv->x;
+    free(temp->suiv);
+    temp->suiv = NULL;
+    return pliste;
+}
 
-maillon_t *supprimer_val(maillon_t *pliste, int val) {}
+maillon_t *supprimer_val(maillon_t *pliste, int val) {
+    maillon_t *temp = pliste;
+
+}
 
 void afficher(maillon_t *pliste) {
     if (pliste != NULL) {
